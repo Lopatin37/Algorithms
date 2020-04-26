e00425762d2ea5f627718201db66268bfc84f8ee
Lopatin37 <Lopatin37@yandex.ru>
2020-04-26 14:37:57 +0300

    Решение предыдущего задания про графы

---
+++ Algorithms/src/main/java/L7/GraphMain.java
@@ -0,0 +1,32 @@
+package L7;
+
+public class GraphMain {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+        graph.addVertex('F');
+        graph.addVertex('G');
+        graph.addVertex('H');
+        graph.addVertex('I');
+        graph.addVertex('J');
+
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 7);
+        graph.addEdge(6, 8);
+        graph.addEdge(7, 9);
+        graph.addEdge(5, 7);
+
+        graph.bfs();
+    }
+
+}
